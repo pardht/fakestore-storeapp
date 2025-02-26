c69945170b893980b0de89d354ff1ead0cfa10e3
ran <randyparadita31@gmail.com>
2025-02-26 17:48:11 +0800

    Enhance SortBtn component to manage sorting state and preserve category in URL

--- src/components/ui/SortBtn.tsx
+++ src/components/ui/SortBtn.tsx
@@ -1,23 +1,35 @@
-"use client"
+"use client";
 
 import Link from 'next/link';
-import { useSearchParams } from 'next/navigation'
-import React from 'react'
+import { useSearchParams } from 'next/navigation';
+import React from 'react';
 
 type SortBtnProps = {
     params: string;
     name: string;
-}
+};
 
 export default function SortBtn({ params, name }: SortBtnProps) {
     const searchParams = useSearchParams();
-    const sort = searchParams.get('sort');
-    const href = sort === name ? '/' : `/?sort=${params}`;
+    const sortInURL = searchParams.get('sort');
+    const cateInURL = searchParams.get('category');
+
+    const isActive = sortInURL === name || (sortInURL === null && name === 'asc');
+
+    const newSearchParams = new URLSearchParams();
+    if (cateInURL) newSearchParams.set('category', cateInURL);
+    if (name === 'desc') newSearchParams.set('sort', params);
+    const href = name === 'asc' ? `/?${newSearchParams.toString()}` : name === 'desc' ? `/?${newSearchParams.toString()}` : ''
+
     return (
         <div>
             <Link href={href}>
-                <button className='bg-violet-500 text-black'>{name}</button>
+                <button
+                    className={`p-3 bg-violet-300 ${isActive ? 'bg-violet-600' : ''}`}
+                >
+                    {name}
+                </button>
             </Link>
         </div>
-    )
+    );
 }
