ac9c6e747fecf4d62739d6d06761d8fba66f0637
ran <randyparadita31@gmail.com>
2025-02-26 18:45:09 +0800

    Add TypeScript types for product and page properties to enhance type safety

---
+++ src/types/index.ts
@@ -0,0 +1,37 @@
+export type HomeProps = {
+    searchParams: Promise<{category?: string}> & Promise<{sort?: string}>
+  };
+
+
+export type Option = {
+    name : string;
+    params : string;
+}
+
+export type ProductType = {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+  };
+
+export type ProductProps = {
+    category?: string;
+    sort?: string;
+  };
+
+export type ProductCardProps = {
+    product: ProductType
+};
+
+export type ProductByIdProps = {
+    params: {
+        id?: string
+    };
+};
+
+export type ProductPageProps = {
+    params: Promise<{id? : string}>
+  }
