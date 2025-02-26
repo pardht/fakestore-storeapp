3df7497bf53b5008afc26f9ffd844dc232e8a244
ran <randyparadita31@gmail.com>
2025-02-26 15:16:09 +0800

    Update searchParams type to include sort parameter in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import Product from "@/components/Product";
 
 type QueryProps = {
-  searchParams: Promise<{category?: string}>;
+  searchParams: Promise<{category?: string}> & Promise<{sort?: string}>
 };
 
 export default async function Home({ searchParams }: QueryProps) {
   const categoryInUrl = (await searchParams).category
+  const sortInUrl = (await searchParams).sort
   return (
     <div className="flex gap-3">
-      <Product category={categoryInUrl} />
+      <Product category={categoryInUrl} sort={sortInUrl}/>
     </div>
   );
 }
