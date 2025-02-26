bc1d80058601cf4674c74dd1d3e89d1959e62b5e
ran <randyparadita31@gmail.com>
2025-02-26 17:48:25 +0800

    Refactor CategoryBtn component to preserve sorting state in URL and improve category link handling

--- src/components/ui/CategoryBtn.tsx
+++ src/components/ui/CategoryBtn.tsx
@@ -9,9 +9,15 @@ type CategoryBtnProps = {
 }
 
 export default function CategoryBtn({ name, params }: CategoryBtnProps) {
-  const getCateInUrl = useSearchParams();
-  const cate = getCateInUrl.get('category');
-  const href = name === cate ? '/' : `/?category=${params}`;
+  const searchParams = useSearchParams();
+  const cateInURL = searchParams.get('category');
+  const sortInURL = searchParams.get('sort');
+
+  const newSearchParams = new URLSearchParams();
+  if (sortInURL) newSearchParams.set('sort', sortInURL);
+  if (name !== cateInURL) newSearchParams.set('category', params);
+
+  const href = name === cateInURL ? `/?${newSearchParams.toString()}` : `/?${newSearchParams.toString()}`;
 
   const XLogo = () => {
     return <span className='p-2 bg-red-500 rounded-full text-white'>x</span>
@@ -19,9 +25,9 @@ export default function CategoryBtn({ name, params }: CategoryBtnProps) {
 
   return (
     <Link href={href} >
-      <button className={`rounded-[20px] p-3 bg-blue-200 text-black  hover:bg-blue-500 ${name === cate ? "bg-blue-500" : ""}`}>
+      <button className={`rounded-[20px] p-3 bg-blue-200 text-black  hover:bg-blue-500 ${name === cateInURL ? "bg-blue-500" : ""}`}>
         {name}
-        {name === cate ? <XLogo /> : ""}
+        {name === cateInURL ? <XLogo /> : ""}
       </button>
     </Link>
   )
