816b2bdd8fb09ca4963643d4a2621b6b48591c3e
ran <randyparadita31@gmail.com>
2025-02-26 13:25:50 +0800

    Enhance Product component to display fallback UI when no products are available

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,29 +1,32 @@
-import React from 'react'
-import ProductCard from './ui/ProductCard'
+import React from 'react';
+import ProductCard from './ui/ProductCard';
 import { getAllProductImproved } from '@/lib/fetch';
 
 type QueryProps = {
-    category?: string
-}
+  category?: string;
+};
 
 type ProductType = {
-        id: number,
-        title: string,
-        price: number,
-        description: string,
-        category: string,
-        image: string
-}
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+};
 
-export default async function Product({category}: QueryProps) {
-    const products: ProductType[] = await getAllProductImproved(category);
+export default async function Product({ category }: QueryProps) {
+  const products: ProductType[] = await getAllProductImproved(category);
 
-
-    return (
-            <div className='w-full h-auto grid grid-cols-4 gap-4'>
-                {products.map((product) => (
-                    <ProductCard key={product.id} product={product} />
-                ))}
-            </div>
-    )
+  return (
+    <div className="w-full h-auto grid grid-cols-4 gap-4">
+      {products.length > 0 ? (
+        products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))
+      ) : (
+        <p>No products available</p> // Fallback UI
+      )}
+    </div>
+  );
 }
