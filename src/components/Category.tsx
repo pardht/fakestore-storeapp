a5876d9e6a3d3addfae1392c60ab9b8488a9f3b3
ran <randyparadita31@gmail.com>
2025-02-26 13:26:05 +0800

    Add loading state for categories in Category component

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { getAllCategory } from '@/lib/fetch';
 import CategoryBtn from './ui/CategoryBtn';
 
-export default async function Category( ) {
+function categoryLoading() {
+    return (
+        <div>
+            Loading Categories...
+        </div>
+    )
+}
+
+export default async function Category() {
     const category: string[] = await getAllCategory();
-   
-    // const category = ['electronics', 'jewelery',"men's clothing","women's clothing"]
     return (
         <div className='p-3 flex flex-col gap-3 '>
             Category :
+            <Suspense fallback={categoryLoading()}>
                 {category.map((category) => (
-                    <CategoryBtn key={category} params={category} name={category}  />
+                    <CategoryBtn key={category} params={category} name={category} />
                 ))}
+            </Suspense>
         </div>
     );
 }
