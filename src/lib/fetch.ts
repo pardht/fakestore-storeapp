af025b90c7b1d16f6caa1e00dd8629ac517de0f8
ran <randyparadita31@gmail.com>
2025-02-27 18:45:41 +0800

    Add limit parameter to getAllProductImproved function for enhanced data fetching

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,19 +1,22 @@
 import { ProductProps, ProductType} from "@/types";
 
-
 export async function getProductById(id:string): Promise<ProductType>{
     const res = await fetch(`https://fakestoreapi.com/products/${id}`);
     return res.json()
 }
 
-export async function getAllProductImproved({ category, sort }: ProductProps): Promise<ProductType[]> {
+export async function getAllProductImproved({ category, sort, limit }: ProductProps): Promise<ProductType[]> {
     let url = 'https://fakestoreapi.com/products';
     if (category) {
       url += `/category/${encodeURIComponent(category)}`;
     }
     if (sort) {
-      url += `${category ? '?' : '?'}sort=${sort}`;
+      url += `${limit && category ? '?' : '?' }sort=${sort}`;
     }
+    if(limit) {
+      url += `${sort && category ? '&' : '?' }limit=${limit}`;
+    }
+    console.log('infetch', url)
     const res = await fetch(url);
     if (!res.ok) {
       throw new Error(`HTTP Error bang! Status ${res.status}`);
