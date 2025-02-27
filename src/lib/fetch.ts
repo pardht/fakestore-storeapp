9f96818b9fa039becc37a8165e9554b7889f0935
ran <randyparadita31@gmail.com>
2025-02-27 14:42:51 +0800

    Refactor fetch functions to TypeScript and improve URL handling for category and sort options

---
+++ src/lib/fetch.ts
@@ -0,0 +1,22 @@
+import { ProductProps, ProductType} from "@/types";
+
+
+export async function getProductById(id:string): Promise<ProductType>{
+    const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+    return res.json()
+}
+
+export async function getAllProductImproved({ category, sort }: ProductProps): Promise<ProductType[]> {
+    let url = 'https://fakestoreapi.com/products';
+    if (category) {
+      url += `/category/${encodeURIComponent(category)}`;
+    }
+    if (sort) {
+      url += `${category ? '?' : '?'}sort=${sort}`;
+    }
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`HTTP Error bang! Status ${res.status}`);
+    }
+    return res.json();
+  }
