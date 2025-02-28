65ffa75d1d1c786c0a6284cbf8374b1a54679f38
ran <randyparadita31@gmail.com>
2025-02-28 14:04:24 +0800

    Update layout title and remove unused SideBar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import TopNavbar from "@/components/TopNavbar";
-import SideBar from "@/components/SideBar";
-
-
 
 export const metadata: Metadata = {
-  title: "fekstor",
+  title: "freakystore",
   description: "ss",
-};
+};  
 
 export default function RootLayout({
   children,
@@ -20,7 +17,6 @@ export default function RootLayout({
       <body className="bg-[#f0ff8f] flex flex-col items-center">
         <TopNavbar />
         <div className="w-full max-w-[1200px] flex gap-3">
-        <SideBar/>
         {children}
         </div>
       </body>
