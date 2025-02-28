f65b9019e43980eaf602c5f4b07d601d5c4d81a7
ran <randyparadita31@gmail.com>
2025-02-28 12:27:00 +0800

    Refactor LimitBtn component for improved limit parameter handling and active state styling

--- src/components/ui/LimitBtn.tsx
+++ src/components/ui/LimitBtn.tsx
@@ -10,15 +10,19 @@ export default function LimitBtn({ params, name }: Option) {
     const sortInUrl = searchParams.get('sort');
     const limitInUrl = searchParams.get('limit');
 
+    const isActive = params === limitInUrl || (limitInUrl === null && params === 'all');
+
     const newSearchParams = new URLSearchParams();
     if (cateInUrl) newSearchParams.set('category', cateInUrl);
     if (sortInUrl) newSearchParams.set('sort', sortInUrl);
-    if (params !== limitInUrl) newSearchParams.set('limit', params);
+    if (params === '4' || '8' || '16') newSearchParams.set('limit', params);
+    if (params === 'all') newSearchParams.delete('limit');
+
     const href = params === limitInUrl ? `/?${newSearchParams.toString()}` : `/?${newSearchParams.toString()}`;
     return (
         <div>
             <Link href={href}>
-                <button className={`p-3 bg-amber-300 text-black ${params === limitInUrl ? 'bg-amber-800' : ''}`}>{name}</button>
+                <button className={`p-3 bg-amber-300 text-black ${isActive ? 'bg-amber-800' : ''}`}>{name}</button>
             </Link>
         </div>
     )
