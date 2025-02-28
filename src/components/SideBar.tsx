e06adf97ae5b352bc36bf8fa706aeaf210be8b01
ran <randyparadita31@gmail.com>
2025-02-28 21:26:30 +0800

    Replace Category component with Filter in SideBar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Category from './Category'
+import Filter from './Filter'
 
 export default function SideBar() {
   return (
     <div className='p-2  w-[400px]  h-auto'>
         <div className='  h-full  bg-pink-400'>this is SideBar
-            <Category/>
+            <Filter/>
         </div>
     </div>
   )
