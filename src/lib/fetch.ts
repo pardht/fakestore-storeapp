3b579d0b368ef9877996262ef95c796cc60c0bd4
ran <randyparadita31@gmail.com>
2025-02-28 13:36:06 +0800

    Remove debug console log from getAllProductImproved function

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,25 +1,24 @@
-import { ProductProps, ProductType} from "@/types";
+import { ProductProps, ProductType } from "@/types";
 
-export async function getProductById(id:string): Promise<ProductType>{
-    const res = await fetch(`https://fakestoreapi.com/products/${id}`);
-    return res.json()
+export async function getProductById(id: string): Promise<ProductType> {
+  const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+  return res.json()
 }
 
 export async function getAllProductImproved({ category, sort, limit }: ProductProps): Promise<ProductType[]> {
-    let url = 'https://fakestoreapi.com/products';
-    if (category) {
-      url += `/category/${encodeURIComponent(category)}`;
-    }
-    if (sort) {
-      url += `${limit && category ? '?' : '?' }sort=${sort}`;
-    }
-    if(limit) {
-      url += `${sort && category ? '&' : sort ? '&' : '?'}limit=${limit}`;
-    }
-    console.log('infetch', url)
-    const res = await fetch(url);
-    if (!res.ok) {
-      throw new Error(`HTTP Error bang! Status ${res.status}`);
-    }
-    return res.json();
+  let url = 'https://fakestoreapi.com/products';
+  if (category) {
+    url += `/category/${encodeURIComponent(category)}`;
   }
+  if (sort) {
+    url += `${limit && category ? '?' : '?'}sort=${sort}`;
+  }
+  if (limit) {
+    url += `${sort && category ? '&' : sort ? '&' : '?'}limit=${limit}`;
+  }
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`HTTP Error bang! Status ${res.status}`);
+  }
+  return res.json();
+}
