8f51d988dac5d8eb12db3aa4621210fe423f1290
ran <randyparadita31@gmail.com>
2025-03-01 21:06:56 +0800

    Add SearchBar component and integrate search parameter in Product component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Product from "@/components/Product";
 import SideBar from "@/components/SideBar";
 import LoadProduct from "@/components/ui/LoadProduct";
 import ProductsCarousel from "@/components/ui/ProductsCarousel";
+import SearchBar from "@/components/ui/SearchBar";
 import { HomeProps } from "@/types";
 import { Suspense } from "react";
 
@@ -9,13 +10,16 @@ export default async function Home({ searchParams }: HomeProps) {
   const categoryInUrl = (await searchParams).category
   const sortInUrl = (await searchParams).sort
   const limitInUrl = (await searchParams).limit
+  const searchInUrl = (await searchParams).search
+  console.log('in page', searchInUrl)
   return (
     <div className="flex flex-col gap-3">
         <ProductsCarousel />
+        <SearchBar/>
       <div className="flex ">
         <SideBar />
         <Suspense fallback={<LoadProduct />}>
-          <Product category={categoryInUrl} sort={sortInUrl} limit={limitInUrl} />
+          <Product category={categoryInUrl} sort={sortInUrl} limit={limitInUrl} search={searchInUrl} />
         </Suspense>
       </div>
     </div>
