43a3c315facea30441557e63fd82c60f8c470ddc
ran <randyparadita31@gmail.com>
2025-03-03 22:23:44 +0800

    Add Satoshi font to the project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        satoshi: ["Satoshi", "sans-serif"],
+      }
     },
   },
   plugins: [],
