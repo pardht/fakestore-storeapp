2fa39f83b232d41feec124894c9a646181f5bbdc
ran <randyparadita31@gmail.com>
2025-03-04 18:02:32 +0800

    Refactor CategoryBtn component to use icons for active state indication

--- src/components/ui/CategoryBtn.tsx
+++ src/components/ui/CategoryBtn.tsx
@@ -2,21 +2,19 @@
 import Link from 'next/link'
 import { Option } from '@/types/index';
 import { useFilterParams, updateFilterParams } from '@/lib/filterUtils';
+import { MdRadioButtonUnchecked } from 'react-icons/md';
+import { IoIosCloseCircle } from 'react-icons/io';
 
 export default function CategoryBtn({ name, params }: Option) {
   const currentParams = useFilterParams();
   const isActive = currentParams.category === params;
   const href = updateFilterParams(currentParams, 'category', isActive ? undefined : params);
 
-  const XLogo = () => {
-    return <span className='p-2 bg-red-500 rounded-full text-white'>x</span>
-  }
-
   return (
     <Link href={href} >
-      <button className={`rounded-[20px] p-3 bg-blue-200 text-black  hover:bg-blue-500 ${params === currentParams.category ? "bg-blue-500" : ""}`}>
+      <button className={`flex items-center p-3 gap-[5px]`}>
+        {isActive ? <IoIosCloseCircle size={25} className='fill-[#06D6A0]'/> : <MdRadioButtonUnchecked size={25} className='fill-[#06D6A0]'/>}
         {name}
-        {params === currentParams.category ? <XLogo /> : ""}
       </button>
     </Link>
   )
