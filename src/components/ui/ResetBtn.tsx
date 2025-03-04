5b08e1797e4996140484cd2cd4699f35edfdaa04
ran <randyparadita31@gmail.com>
2025-03-04 18:02:45 +0800

    Add ResetBtn component for resetting filters with icon indication

---
+++ src/components/ui/ResetBtn.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React from 'react'
+import { useFilterParams } from '@/lib/filterUtils';
+import { RiResetLeftFill } from 'react-icons/ri';
+
+export default function ResetBtn() {
+  const currentParams = useFilterParams()
+  const hasFilter = currentParams.category || currentParams.sort || currentParams.limit
+
+  const href = currentParams.search ? '/?search=' + currentParams.search : '/'
+  return (
+    <Link href={href}>
+      <button>
+        {hasFilter ? <RiResetLeftFill size={25} className='fill-[#06D6A0]' /> : null}
+      </button>
+    </Link>
+  )
+}
