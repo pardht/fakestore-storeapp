ff75a796beb5e9f0fcd5195df57a99417fa09e75
ran <randyparadita31@gmail.com>
2025-03-04 18:01:12 +0800

    Replace empty product message with NotFound component for better user experience

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -3,19 +3,20 @@ import ProductCard from './ui/ProductCard';
 import { getAllProductImproved } from '@/lib/fetch';
 import { ProductType, } from '@/types/index';
 import { ProductProps } from '@/types/index';
+import NotFound from './ui/NotFound';
 
 export default async function Product({ category, sort, limit, search }: ProductProps) {
-  const products: ProductType[] = await getAllProductImproved({category, sort, limit, search});
+  const products: ProductType[] = await getAllProductImproved({ category, sort, limit, search });
 
   return (
-    <div className="w-full h-auto grid grid-cols-4 gap-4">
-      {products.length > 0 ? (
-        products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))
-      ) : (
-        <p className='bg-sky-300 text-black'>gak nemu product gw </p>
-      )}
-    </div>
+      <div className="w-full h-auto px-2 bg-white grid grid-cols-2 gap-3 md:grid-cols-4 md:gap-5">
+        {products.length > 0 ? (
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))
+        ) : (
+          <NotFound />
+        )}
+      </div>
   );
 }
