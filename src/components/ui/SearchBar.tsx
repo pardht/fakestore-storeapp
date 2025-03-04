388ac6bc9e798831d16166c56071f65b6339d321
ran <randyparadita31@gmail.com>
2025-03-04 18:02:52 +0800

    Enhance SearchBar component with search icon and improved styling

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { updateFilterParams, useFilterParams } from '@/lib/filterUtils';
+import { CiSearch } from 'react-icons/ci';
 
 export default function SearchBar() {
   const { search } = useFilterParams();
@@ -15,20 +16,22 @@ export default function SearchBar() {
     router.push(newUrl);
   };
 
-  useEffect (()=>{
+  useEffect(() => {
     handleSearch()
   }, [query])
 
-  console.log('insearch barr', query)
   return (
-    <div>
-      <input
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder="Search"
-        className="p-2 border text-black"
-      />
+    <div className='w-full px-5'>
+        <div className='py-[5px] px-3 flex items-center shadow-black/10 shadow-md rounded-[15px] '>
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Search"
+          className="w-full p-2 rounded-[10px] text-black outline-none"
+        />
+        <CiSearch size={25} />
+      </div>
     </div>
   );
 }
