b6bc7bbafc075d7ca51e7e81172de81d004e1c36
ran <randyparadita31@gmail.com>
2025-03-04 18:18:46 +0800

    Refactor ProductCard component for improved layout and structure

--- src/components/ui/ProductCard.tsx
+++ src/components/ui/ProductCard.tsx
@@ -5,24 +5,26 @@ import { ProductCardProps } from '@/types/index';
 
 export default function ProductCard({ product }: ProductCardProps) {
     return (
-        <Link href={`product/${product.id}`}>
-            <div className='w-full h-full pb-[7rem] relative bg-white rounded-[10px] shadow-lg group transition-all brightness-[0.95] hover:shadow-xl'>
-                <div className='w-full h-[50%] bottom-0 p-3 md:p-5 absolute flex flex-col justify-end rounded-[10px]'>
-                    <div className='md:text-xl font-light transition line-clamp-2 md:line-clamp-none md:truncate md:group-hover:line-clamp-2  group-hover:whitespace-normal'>
-                        {product.title}
+        <div>
+            <Link href={`product/${product.id}`} className='bg-black'>
+                <div className='w-full h-auto pb-[7rem] relative bg-white rounded-[10px] shadow-lg group transition-all brightness-[0.95] hover:shadow-xl'>
+                    <div className='w-full h-[50%] bottom-0 p-3 md:p-5 absolute flex flex-col justify-end rounded-[10px]'>
+                        <div className='md:text-xl font-light transition line-clamp-2 md:line-clamp-none md:truncate md:group-hover:line-clamp-2  group-hover:whitespace-normal'>
+                            {product.title}
+                        </div>
+                        <div className='text-sm md:text-base font-light text-zinc-400'>
+                            {product.category}
+                        </div>
+                        <div className='text-sm md:text-lg font-bold'>
+                            ${product.price}
+                        </div>
                     </div>
-                    <div className='text-sm md:text-base font-light text-zinc-400'>
-                        {product.category}
-                    </div>
-                    <div className='text-sm md:text-lg font-bold'>
-                        ${product.price}
+                    <div className='p-6 md:p-10'>
+                        <Image src={product.image} alt='ProductImage' width={100} height={100} priority={true}
+                            className='w-20 h-20 md:w-40 md:h-48 mx-auto scale-100 transition-all group-hover:scale-[1.03]' />
                     </div>
                 </div>
-                <div className='p-6 md:p-10'>
-                    <Image src={product.image} alt='ProductImage' width={100} height={100} priority={true}
-                        className='w-20 h-20 md:w-40 md:h-48 mx-auto scale-100 transition-all group-hover:scale-[1.03]' />
-                </div>
-            </div>
-        </Link>
+            </Link>
+        </div>
     )
 }
