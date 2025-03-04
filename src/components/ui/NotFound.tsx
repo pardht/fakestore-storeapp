af6457d94596bb74f2c2e49647e430c915d61757
ran <randyparadita31@gmail.com>
2025-03-04 18:01:04 +0800

    Add NotFound component for handling missing products

---
+++ src/components/ui/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function NotFound() {
+    return (
+        <div className='col-span-2 w-full h-screen bg-white flex justify-center items-center p-5 text-center'>
+            <p>
+                Sorry we {`couldn't`} find the product you are looking for
+            </p>
+        </div>
+    )
+}
