164f33c7535ee56040b1b16675f4641fc869212f
ran <randyparadita31@gmail.com>
2025-03-05 08:40:07 +0800

    Enhance NotFound component to accept a prop for dynamic messaging

--- src/components/ui/NotFound.tsx
+++ src/components/ui/NotFound.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
-export default function NotFound() {
+type prop= {
+    prop?: string
+}
+
+export default function NotFound({prop}:prop) {
     return (
-        <div className='col-span-2 w-full h-screen bg-white flex justify-center items-center p-5 text-center'>
-            <p>
-                Sorry we {`couldn't`} find the product you are looking for
+        <div className='col-span-full w-full h-screen bg-white flex justify-center items-center p-5 text-center'>
+            <p className='md:text-lg'>
+                Sorry, we {`couldn't`} find a product that rhymes with {`"${prop}"`}
             </p>
         </div>
     )
