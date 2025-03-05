346f3f02666d1cead6a271ee5d7dcab1ae9740b1
ran <randyparadita31@gmail.com>
2025-03-05 22:57:55 +0800

    Removing unused import

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import ProductCard from './ui/ProductCard';
 import { getAllProductImproved } from '@/lib/fetch';
 import { ProductType, } from '@/types/index';
