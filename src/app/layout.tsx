2168cfb0dc323e7ecedd24c937189b65baa151bb
ran <randyparadita31@gmail.com>
2025-03-05 16:40:15 +0800

    Add Footer component to RootLayout and adjust body styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import TopNavbar from "@/components/TopNavbar";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "freakystore",
   description: "ss",
-};  
+};
 
 export default function RootLayout({
   children,
@@ -14,10 +15,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className=" flex flex-col items-center">
-        <TopNavbar />
+      <body className="w-full max-w-[1400px] flex flex-col mx-auto px-4">
+            <TopNavbar />
         <div className="w-full max-w-[1400px] flex gap-3">
-        {children}
+          {children}
+        </div>
+        <div className="py-8">
+        <Footer />
         </div>
       </body>
     </html>
