0a0c1d3c05bc62f4b44bf7a7af08251a54971b7b
ran <randyparadita31@gmail.com>
2025-03-05 12:44:04 +0800

    Update ProductCard component to improve layout and image handling

--- src/components/ui/ProductCard.tsx
+++ src/components/ui/ProductCard.tsx
@@ -7,7 +7,7 @@ export default function ProductCard({ product }: ProductCardProps) {
     return (
         <div>
             <Link href={`product/${product.id}`} className='bg-black'>
-                <div className='w-full h-auto pb-[7rem] relative bg-white rounded-[10px] shadow-lg group transition-all brightness-[0.95] hover:shadow-xl'>
+                <div className='w-full h-full max-h-[23rem] pb-[7rem] relative bg-white rounded-[10px] shadow-lg group transition-all brightness-[0.95] hover:shadow-xl'>
                     <div className='w-full h-[50%] bottom-0 p-3 md:p-5 absolute flex flex-col justify-end rounded-[10px]'>
                         <div className='md:text-xl font-light transition line-clamp-2 md:line-clamp-none md:truncate md:group-hover:line-clamp-2  group-hover:whitespace-normal'>
                             {product.title}
@@ -19,9 +19,9 @@ export default function ProductCard({ product }: ProductCardProps) {
                             ${product.price}
                         </div>
                     </div>
-                    <div className='p-6 md:p-10'>
+                    <div className='p-6 md:p-10 h-full flex items-center'>
                         <Image src={product.image} alt='ProductImage' width={100} height={100} priority={true}
-                            className='w-20 h-20 md:w-40 md:h-48 mx-auto scale-100 transition-all group-hover:scale-[1.03]' />
+                            className='w-20 md:w-40 md:max-h-[12rem] mx-auto scale-100 transition-all group-hover:scale-[1.03] overflow-visible' />
                     </div>
                 </div>
             </Link>
