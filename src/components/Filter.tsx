d927e4bb74e731f63467e84a4910ea66464be59c
ran <randyparadita31@gmail.com>
2025-03-05 22:54:46 +0800

    Refactor Filter component for improved layout and spacing

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -29,16 +29,16 @@ export default function Filter() {
     }
     return (
         <>
-            <div className='flex justify-between  p-5'>
+            <div className='flex justify-between pb-5'>
                 <button onClick={handleOpen} className='flex items-center gap-[4px] bg-white text-lg font-semibold'>
                     <p> Filter </p>
                     <LuSettings2 />
                 </button>
                 <ResetBtn />
             </div>
-            <div className={`px-5 transition-all duration-1000 md:h-auto ${isOpen ? 'h-[600px] ' : 'h-0'}`}>
+            <div className={`transition-all duration-1000 md:h-auto ${isOpen ? 'h-[600px] ' : 'h-0'}`}>
                 <Suspense fallback={filterLoading()}>
-                    <div className='pb-6 border-b'>
+                    <div className='px-4 pb-6 border-b'>
                         <p className='pb-5 font-medium'>Category {currentParams.category ? <span className='font-light text-zinc-400'>/ {currentParams.category}</span> : ''}</p>
                         <div className='space-y-3 flex flex-col'>
                             {categories.map((category) => (
@@ -46,7 +46,7 @@ export default function Filter() {
                             ))}
                         </div>
                     </div>
-                    <div className='py-6 border-b'>
+                    <div className='px-4 py-6 border-b'>
                         <p className='pb-5 font-medium'>Sort by {currentParams.sort ? <span className='font-light text-zinc-400'>/ {currentParams.sort}</span> : <span className='font-light text-zinc-400'>/ asc</span>}</p>
                         <div className='space-y-3 flex flex-col'>
                             {sorts.map((sort) => (
@@ -54,7 +54,7 @@ export default function Filter() {
                             ))}
                         </div>
                     </div>
-                    <div className='py-6 border-b'>
+                    <div className='px-4 py-6 border-b'>
                         <p className='pb-5 font-medium'>Amount Product {currentParams.limit ? <span className='font-light text-zinc-400'>/ {currentParams.limit}</span> : <span className='font-light text-zinc-400'>/ all</span>}</p>
                         <div className='flex'>
                             {limits.map((limit) => (
