0ece0fc8ff6aa54f7818ea26abdfb2777d07d295
ran <randyparadita31@gmail.com>
2025-03-05 16:40:57 +0800

    Rename Product component to ProductPage

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ProductCard from './ui/ProductCard';
 import { getAllProductImproved } from '@/lib/fetch';
 import { ProductType, } from '@/types/index';
 import { ProductProps } from '@/types/index';
 import NotFound from './ui/NotFound';
+import ProductCarousel from './ui/ProductsCarousel';
 
-export default async function Product({ category, sort, limit, search }: ProductProps) {
+export default async function ProductPage({ category, sort, limit, search }: ProductProps) {
   const products: ProductType[] = await getAllProductImproved({ category, sort, limit, search });
 
   return (
-      <div className="w-full min-h-screen h-auto px-2 bg-white grid grid-cols-2 gap-3 md:grid-cols-4 md:gap-5">
+      <div className="w-full h-full bg-white grid grid-cols-2 gap-3 md:grid-cols-4 md:gap-5">
         {products.length > 0 ? (
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))
         ) : (
-          <NotFound />
+          <NotFound prop={search}/>
         )}
       </div>
   );
