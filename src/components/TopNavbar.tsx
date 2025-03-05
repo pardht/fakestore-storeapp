1c25c4c27fcdbae28171d0a1e7b977f53c44ee55
ran <randyparadita31@gmail.com>
2025-03-05 17:06:42 +0800

    Remove padding from TopNavbar for improved layout consistency

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function TopNavbar() {
   return (
-    <div className='w-full p-2'>
+    <div className='w-full'>
         <div className='w-full h-[90px] bg-amber-500 justify-center text-lg flex items-center'>this is top navbar</div>
     </div>
   )
