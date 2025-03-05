55a92cb0e59b4b742fb9e35accb9d055128f177e
ran <randyparadita31@gmail.com>
2025-03-05 17:04:47 +0800

    Update ProductCard component styling for improved visual consistency

--- src/components/ui/ProductCard.tsx
+++ src/components/ui/ProductCard.tsx
@@ -6,8 +6,8 @@ import { ProductCardProps } from '@/types/index';
 export default function ProductCard({ product }: ProductCardProps) {
     return (
         <div>
-            <Link href={`product/${product.id}`} className='bg-black'>
-                <div className='w-full h-full max-h-[23rem] pb-[7rem] relative bg-white rounded-[10px] shadow-lg group transition-all brightness-[0.95] hover:shadow-xl'>
+            <Link href={`product/${product.id}`}>
+                <div className='w-full h-full max-h-[23rem] pb-[7rem] relative bg-white rounded-[10px] shadow-xl shadow-[#456d62]/10 group transition-all brightness-[0.95]'>
                     <div className='w-full h-[50%] bottom-0 p-3 md:p-5 absolute flex flex-col justify-end rounded-[10px]'>
                         <div className='md:text-xl font-light transition line-clamp-2 md:line-clamp-none md:truncate md:group-hover:line-clamp-2  group-hover:whitespace-normal'>
                             {product.title}
