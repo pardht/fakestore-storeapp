8f222d3f6b13365793a43463255f73fe3ecd68e8
ran <randyparadita31@gmail.com>
2025-03-05 23:31:54 +0800

    Removing Suspense

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import React, { Suspense, useState } from 'react'
+import React, { useState } from 'react';
 import CategoryBtn from './ui/CategoryBtn';
 import SortBtn from './ui/SortBtn';
 import LimitBtn from './ui/LimitBtn';
@@ -11,59 +11,49 @@ import { useFilterParams } from '@/lib/filterUtils';
 import { LuSettings2 } from 'react-icons/lu';
 import ResetBtn from './ui/ResetBtn';
 
-
-function filterLoading() {
-    return (
-        <div>
-            Loading Categories...
-        </div>
-    )
-}
-
 export default function Filter() {
-    const currentParams = useFilterParams();
-    const [isOpen, setIsOpen] = useState(false);
+  const currentParams = useFilterParams();
+  const [isOpen, setIsOpen] = useState(false);
 
-    const handleOpen = () => {
-        setIsOpen(!isOpen)
-    }
-    return (
-        <>
-            <div className='flex justify-between pb-5'>
-                <button onClick={handleOpen} className='flex items-center gap-[4px] bg-white text-lg font-semibold'>
-                    <p> Filter </p>
-                    <LuSettings2 />
-                </button>
-                <ResetBtn />
-            </div>
-            <div className={`transition-all duration-1000 md:h-auto ${isOpen ? 'h-[600px] ' : 'h-0'}`}>
-                <Suspense fallback={filterLoading()}>
-                    <div className='px-4 pb-6 border-b'>
-                        <p className='pb-5 font-medium'>Category {currentParams.category ? <span className='font-light text-zinc-400'>/ {currentParams.category}</span> : ''}</p>
-                        <div className='space-y-3 flex flex-col'>
-                            {categories.map((category) => (
-                                <CategoryBtn key={category.params} params={category.params} name={category.name} />
-                            ))}
-                        </div>
-                    </div>
-                    <div className='px-4 py-6 border-b'>
-                        <p className='pb-5 font-medium'>Sort by {currentParams.sort ? <span className='font-light text-zinc-400'>/ {currentParams.sort}</span> : <span className='font-light text-zinc-400'>/ asc</span>}</p>
-                        <div className='space-y-3 flex flex-col'>
-                            {sorts.map((sort) => (
-                                <SortBtn key={sort.params} params={sort.params} name={sort.name} />
-                            ))}
-                        </div>
-                    </div>
-                    <div className='px-4 py-6 border-b'>
-                        <p className='pb-5 font-medium'>Amount Product {currentParams.limit ? <span className='font-light text-zinc-400'>/ {currentParams.limit}</span> : <span className='font-light text-zinc-400'>/ all</span>}</p>
-                        <div className='flex'>
-                            {limits.map((limit) => (
-                                <LimitBtn key={limit.params} params={limit.params} name={limit.name} />
-                            ))}
-                        </div>
-                    </div>
-                </Suspense>
-            </div>
-        </>
-    );
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <div className='flex justify-between pb-5'>
+        <button onClick={handleOpen} className='flex items-center gap-[4px] bg-white text-lg font-semibold'>
+          <p> Filter </p>
+          <LuSettings2 />
+        </button>
+        <ResetBtn />
+      </div>
+      <div className={`transition-all duration-1000 md:h-auto ${isOpen ? 'h-[600px] ' : 'h-0'}`}>
+        <div className='px-4 pb-6 border-b'>
+          <p className='pb-5 font-medium'>Category {currentParams.category ? <span className='font-light text-zinc-400'>/ {currentParams.category}</span> : ''}</p>
+          <div className='space-y-3 flex flex-col'>
+            {categories.map((category) => (
+              <CategoryBtn key={category.params} params={category.params} name={category.name} />
+            ))}
+          </div>
+        </div>
+        <div className='px-4 py-6 border-b'>
+          <p className='pb-5 font-medium'>Sort by {currentParams.sort ? <span className='font-light text-zinc-400'>/ {currentParams.sort}</span> : <span className='font-light text-zinc-400'>/ asc</span>}</p>
+          <div className='space-y-3 flex flex-col'>
+            {sorts.map((sort) => (
+              <SortBtn key={sort.params} params={sort.params} name={sort.name} />
+            ))}
+          </div>
+        </div>
+        <div className='px-4 py-6 border-b'>
+          <p className='pb-5 font-medium'>Amount Product {currentParams.limit ? <span className='font-light text-zinc-400'>/ {currentParams.limit}</span> : <span className='font-light text-zinc-400'>/ all</span>}</p>
+          <div className='flex'>
+            {limits.map((limit) => (
+              <LimitBtn key={limit.params} params={limit.params} name={limit.name} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
