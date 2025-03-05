6ff616040e3dd7c9e3ce6c309591bd795501adcd
ran <randyparadita31@gmail.com>
2025-03-05 16:41:47 +0800

    Add product page component in /product folder

---
+++ src/app/product/page.tsx
@@ -0,0 +1,27 @@
+import ProductPage from "@/components/ProductPage";
+import SideBar from "@/components/SideBar";
+import LoadProduct from "@/components/ui/LoadProduct";
+import { HomeProps } from "@/types";
+import { Suspense } from "react";
+
+export default async function page({ searchParams }: HomeProps) {
+    const categoryInUrl = (await searchParams).category
+    const sortInUrl = (await searchParams).sort
+    const limitInUrl = (await searchParams).limit
+    const searchInUrl = (await searchParams).search
+    console.log('in page', searchInUrl)
+    return (
+        <div className="w-full h-auto flex flex-col gap-3">
+            <div className="flex flex-col md:grid md:grid-cols-8 ">
+                <div className="w-full col-span-2 md:relative">
+                    <SideBar />
+                </div>
+                <div className="col-span-6">
+                    <Suspense fallback={<LoadProduct />}>
+                        <ProductPage category={categoryInUrl} sort={sortInUrl} limit={limitInUrl} search={searchInUrl} />
+                    </Suspense>
+                </div>
+            </div>
+        </div>
+    );
+}
