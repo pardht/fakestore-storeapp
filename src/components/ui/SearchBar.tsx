ed57364b0c737de462498e4ccbed02d2b90c3b24
ran <randyparadita31@gmail.com>
2025-03-05 22:54:00 +0800

    Refactor SearchBar component for improved search functionality and clear button interaction

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -11,27 +11,41 @@ export default function SearchBar() {
   const router = useRouter();
   const [query, setQuery] = useState(search || '');
 
-  const handleSearch = () => {
-    const newUrl = query ? '/product' + updateFilterParams(currentParams, 'search', query) : '/product'
+  const handleClear = () => {
+    setQuery('');
+    router.push('/product');
+  };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newQuery = e.target.value;
+    setQuery(newQuery);
+    const newUrl = newQuery 
+      ? `/product${updateFilterParams(currentParams, 'search', newQuery)}` 
+      : '/product';
     router.push(newUrl);
   };
 
   useEffect(() => {
-      handleSearch()
-  }, [query])
-  
+    setQuery(currentParams.search || '');
+  }, [currentParams.search]);
+
   return (
-    <div className='md:w-[300px] '>
-        <div className='py-[5px] px-3 flex items-center shadow-black/10 shadow-md rounded-[15px] '>
+    <div className='w-full'>
+      <div className='md:py-[5px] px-3 flex items-center bg-white shadow-black/10 shadow-sm rounded-[15px] '>
         <input
           type="text"
           value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          placeholder="Search"
-          className="w-full p-2 rounded-[10px] text-black outline-none"
+          onChange={handleChange}
+          placeholder="Looking for something specific?"
+          className="w-full p-2 rounded-[10px] text-black outline-none placeholder:tracking-wide"
         />
-        <CiSearch size={25} />
+        {query === '' ? <CiSearch size={25} /> : 
+        <button onClick={handleClear}>
+          <p className='font-medium hover:opacity-70'>Clear</p>
+        </button>
+       }
+
       </div>
     </div>
   );
 }
+
