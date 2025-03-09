e4695460ca752428dd239962a1343190e76225d4
ran <randyparadita31@gmail.com>
2025-03-09 21:24:08 +0800

    Add similar product retrieval to ProductPage component

--- src/components/ProductById.tsx
+++ src/components/ProductById.tsx
@@ -1,11 +1,14 @@
-import { getProductById } from "@/lib/fetch";
+import { getProductById, getSimilarProduct } from "@/lib/fetch";
 import Image from "next/image";
 import BackBtn from "./ui/BackBtn";
-import { ProductByIdProps } from "@/types/index";
+import { ProductByIdProps, ProductType } from "@/types/index";
+import ProductCard from "./ui/ProductCard";
 
 
-export default async function ProductPage({ params } : ProductByIdProps) {
+export default async function ProductPage({ params }: ProductByIdProps) {
     const detail = await getProductById((await params).id);
+    const similarProducts = await getSimilarProduct(detail.category)
+    console.log('higi', similarProducts)
 
     return (
         <div className="p-10 flex flex-col gap-4">
@@ -19,6 +22,12 @@ export default async function ProductPage({ params } : ProductByIdProps) {
                     <p>${detail.price}</p>
                     <p>Category: {detail.category}</p>
                 </div>
+            </div>
+            <p className="font-semibold text-2xl">You might also like</p>
+            <div className="flex gap-4 text-black">
+                {similarProducts.sort(()=> Math.random() - 0.5 ).filter((product:ProductType ) => product.id !== detail.id).slice(0, 4).map((product:ProductType) => (
+                    <ProductCard key={product.id} product={product} />
+                ))}
             </div>
         </div>
     );
