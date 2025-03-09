4b98d031e42fe92130f8b36373583881281e4fec
ran <randyparadita31@gmail.com>
2025-03-09 18:50:56 +0800

    Refactor TopNavbar component to enhance layout and improve navigation links

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -1,7 +1,6 @@
 import React, { Suspense } from 'react'
 import SearchBar from './ui/SearchBar'
 import Link from 'next/link'
-import { IoMdInformationCircleOutline } from 'react-icons/io'
 import { LuShoppingCart } from 'react-icons/lu'
 
 export default function TopNavbar() {
@@ -9,15 +8,23 @@ export default function TopNavbar() {
     <div className='w-full'>
       <div className='w-full h-auto flex flex-col rounded-[10px] shadow-md bg-[#70d6bb]'>
         <div className='flex px-3 pt-2 rounded-t-[10px] justify-between items-center  bg-[#6dcfb5]'>
-          <Link href="/">
-            <p className='text-xl font-semibold tracking-wide text-[#F8FFE5]'>RilStore.</p>
-          </Link>
-          <Link href="/about">
-            <div className='flex items-center text-[#f8ffe5]/60 hover:text-[#F8FFE5] transition-all'>
-              <p className='tracking-wide text-sm '>About Rilstore.</p>
-              <IoMdInformationCircleOutline className='w-[20px] h-[20px] ' />
-            </div>
-          </Link>
+          <div>
+            <Link href="/">
+              <p className='text-xl font-semibold tracking-wide text-[#F8FFE5]'>RilStore.</p>
+            </Link>
+          </div>
+          <div className='flex gap-4 md:gap-8'>
+            <Link href="/product">
+              <div className='flex items-center text-[#f8ffe5]/60 hover:text-[#F8FFE5] transition-all'>
+                <p className='tracking-wide text-xs md:text-sm '>Products</p>
+              </div>
+            </Link>
+            <Link href="/about">
+              <div className='flex items-center text-[#f8ffe5]/60 hover:text-[#F8FFE5] transition-all'>
+                <p className='tracking-wide text-xs md:text-sm '>About Rilstore.</p>
+              </div>
+            </Link>
+          </div>
         </div>
         <div className='flex items-center justify-between gap-3 px-3 pt-2 pb-3'>
           <div className='w-full flex justify-center'>
