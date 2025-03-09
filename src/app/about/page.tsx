d0a02dbfbd3772229c86d30dc7db4d9c8691b980
ran <randyparadita31@gmail.com>
2025-03-09 21:24:13 +0800

    Add About page component with introductory content

---
+++ src/app/about/page.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+export default function page() {
+    return (
+        <div className=''>
+            <p>About Rilstore</p>
+            {/* <p>
+                About Rilstore
+                Welcome to Rilstore – the store that’s obviously not a real store. But hey, don’t let that fool you. We’ve got everything you need (and probably some stuff you don’t) all in one app, thanks to the magic of the FakeStore API.
+
+                So, What’s the Deal?
+                Rilstore is your go-to app for browsing a totally random collection of products that might make you say, “Wait, do I actually need this?” Whether you're window shopping for fun or just procrastinating on something important, we’ve got you covered.
+
+                Why Rilstore?
+                It’s Fake, But It’s Fun: Let’s be honest—this isn’t Amazon, but it’s way cooler because we don’t take ourselves too seriously.
+
+                No Pressure: No pushy salespeople. Just you, your phone, and a bunch of stuff to scroll through.
+
+                Endless Entertainment: Shopping here is like going down a rabbit hole of randomness. You never know what you’ll find!
+
+                Our Totally Serious Mission
+                To bring you joy, one fake product at a time. We’re here to make shopping (or fake shopping) fun, easy, and just a little bit ridiculous.
+
+                Who Made This Madness?
+                That would be this bro right here, – Just a friendly neighborhood developer who thought, “Hey, let’s make a fake store app!” This masterpiece was built using the holy trinity of web development: Next.js, TypeScript, and Tailwind CSS. So, here we are. Enjoy!
+
+                Have Feedback or Want to Connect?
+                We’d love to hear from you! Have feedback for this app or just want to connect with the maker? Feel free to reach out on social media:
+
+                Your Twitter Handle
+
+                Your Instagram Handle
+
+                Your LinkedIn Profile
+
+                Thanks for stopping by—now go have some fun with Rilstore!
+            </p> */}
+        </div>
+    )
+}
