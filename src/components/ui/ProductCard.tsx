8ba8b96e58622340d325a8b36db7c9cd353a407a
ran <randyparadita31@gmail.com>
2025-03-09 23:24:23 +0800

    Refactor ProductCard component to enhance layout and display product ratings

--- src/components/ui/ProductCard.tsx
+++ src/components/ui/ProductCard.tsx
@@ -8,20 +8,34 @@ export default function ProductCard({ product }: ProductCardProps) {
         <div>
             <Link href={`product/${product.id}`}>
                 <div className='w-full h-full max-h-[23rem] pb-[7rem] relative bg-white rounded-[10px] shadow-xl shadow-[#456d62]/10 group transition-all brightness-[0.95]'>
-                    <div className='w-full h-[50%] bottom-0 p-3 md:p-5 absolute flex flex-col justify-end rounded-[10px]'>
-                        <div className='md:text-xl font-light transition line-clamp-2 md:line-clamp-none md:truncate md:group-hover:line-clamp-2  group-hover:whitespace-normal'>
-                            {product.title}
+                    <div className=' h-full flex'>
+                        <div className='w-full pb-2 pt-6 md:py-10 flex items-center'>
+                            <Image src={product.image} alt='ProductImage' width={100} height={100} priority={true}
+                                className='w-20 md:w-40 md:max-h-[12rem] mx-auto scale-100 transition-all group-hover:scale-[1.03] overflow-visible' />
                         </div>
-                        <div className='text-sm md:text-base font-light text-zinc-400'>
-                            {product.category}
+                        <div className='w-full h-full bottom-0  absolute flex flex-col justify-between rounded-[10px]'>
+                            <div className='overflow-clip'>
+                                <div className='pb-12 flex flex-col md:gap-2'>
+                                    <p className={`w-24 md:w-40 text-[10px] md:text-sm font-medium px-[14px] md:px-[20px] ml-[-15px] md:ml-[-20px] -rotate-[35deg] text-white bg-pink-600/80 ${product.rating.count > 400 ? 'block' : 'hidden'}`}>
+                                        Bestseller
+                                    </p>
+                                    <p className={`w-28 md:w-40 text-[10px] md:text-sm font-medium px-[14px] md:px-[20px] ml-[-15px] md:ml-[-20px] -rotate-[35deg] text-white bg-amber-500/80 ${product.rating.rate > 4 ? 'block' : 'hidden'}`}>
+                                        Top Rated
+                                    </p>
+                                </div>
+                            </div>
+                            <div className='p-3 md:p-5'>
+                                <div className='text-sm md:text-lg font-base transition-all line-clamp-2   group-hover:whitespace-normal'>
+                                    {product.title}
+                                </div>
+                                <div className='text-sm md:text-base font-light text-zinc-400'>
+                                    {product.category}
+                                </div>
+                                <div className='text-sm md:text-lg font-bold'>
+                                    ${product.price}
+                                </div>
+                            </div>
                         </div>
-                        <div className='text-sm md:text-lg font-bold'>
-                            ${product.price}
-                        </div>
-                    </div>
-                    <div className='p-6 md:p-10 h-full flex items-center'>
-                        <Image src={product.image} alt='ProductImage' width={100} height={100} priority={true}
-                            className='w-20 md:w-40 md:max-h-[12rem] mx-auto scale-100 transition-all group-hover:scale-[1.03] overflow-visible' />
                     </div>
                 </div>
             </Link>
