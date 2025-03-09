55fe40d88147ae2af03544e770797712d1c17352
ran <randyparadita31@gmail.com>
2025-03-09 21:23:42 +0800

    Add dynamic placeholder to SearchBar and improve clear button visibility

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -4,12 +4,15 @@ import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { updateFilterParams, useFilterParams } from '@/lib/filterUtils';
 import { CiSearch } from 'react-icons/ci';
+import { IoIosCloseCircle } from 'react-icons/io';
+import { placeholderValues } from '@/lib/placeholder';
 
 export default function SearchBar() {
   const { search } = useFilterParams();
   const currentParams = useFilterParams();
   const router = useRouter();
   const [query, setQuery] = useState(search || '');
+  const [placeholder, setPlaceholder] = useState('');
 
   const handleClear = () => {
     setQuery('');
@@ -28,19 +31,26 @@ export default function SearchBar() {
     setQuery(currentParams.search || '');
   }, [currentParams.search]);
 
+  useEffect(()=> {
+    const randomized = placeholderValues.sort(() => Math.random() - 0.5).slice(0, 1)[0]
+    setPlaceholder(randomized)
+  },[])
+  console.log('wdajaw', placeholder)
+
   return (
     <div className='w-full'>
-      <div className='md:py-[5px] px-3 flex items-center bg-white shadow-black/10 shadow-sm rounded-[15px] '>
+      <div className='md:py-[5px] pr-2 md:px-3 flex items-center bg-white shadow-black/10 shadow-sm rounded-[15px] '>
         <input
           type="text"
           value={query}
           onChange={handleChange}
-          placeholder="Looking for something specific?"
-          className="w-full p-2 rounded-[10px] text-black outline-none placeholder:tracking-wide"
+          placeholder={placeholder}
+          className="w-full p-2 rounded-[10px] text-black outline-none placeholder:tracking-wide placeholder:text-sm md:placeholder:text-base"
         />
         {query === '' ? <CiSearch size={25} /> : 
         <button onClick={handleClear}>
-          <p className='font-medium hover:opacity-70'>Clear</p>
+          <p className='hidden md:block font-medium hover:opacity-70'>Clear</p>
+          <IoIosCloseCircle size={20} className='md:hidden text-zinc-500'/>
         </button>
        }
 
