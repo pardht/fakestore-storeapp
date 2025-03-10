786c3a9ab4538173b4e95f331f188ad314683a3d
ran <randyparadita31@gmail.com>
2025-03-10 12:50:30 +0800

    Enhance BackBtn component with icons and improved styling for better user experience

--- src/components/ui/BackBtn.tsx
+++ src/components/ui/BackBtn.tsx
@@ -1,5 +1,7 @@
 "use client"
 import React from 'react'
+import { IoIosArrowBack } from 'react-icons/io';
+import { IoReturnUpBackOutline } from 'react-icons/io5';
 
 export default function BackBtn() { 
     function goBack(){
@@ -7,8 +9,11 @@ export default function BackBtn() {
     }
     return (
         <button onClick={goBack}>
-            <div className='max-w-[80px] text-center p-2 bg-green-600 text-white font-bold rounded-full'>
-                Back
+            <div className='flex items-center justify-center gap-[4px] text-center md:p-4 text-black font-bold rounded-full transition-all group'>
+                <p className='p-3 rounded-full group-hover:bg-zinc-500/10 transition-all'>
+                <IoIosArrowBack  size={20} className=''/>
+                </p>
+                <p className='font-light group-hover:tracking-wide transition-all'>Back</p>
             </div>
         </button>
     )
