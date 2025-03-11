ed0d0018b6b7bc015c1968abc0c6928ad5687fdb
ran <randyparadita31@gmail.com>
2025-03-11 22:17:40 +0800

    Refactor TopNavbar component for improved layout and responsiveness

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -5,28 +5,30 @@ import { LuShoppingCart } from 'react-icons/lu'
 
 export default function TopNavbar() {
   return (
-    <div className='w-full'>
-      <div className='w-full h-auto flex flex-col rounded-[10px] shadow-md bg-[#70d6bb]'>
-        <div className='flex px-3 pt-2 rounded-t-[10px] justify-between items-center  bg-[#6dcfb5]'>
-          <div>
-            <Link href="/">
-              <p className='text-xl font-semibold tracking-wide text-[#F8FFE5]'>RilStore.</p>
-            </Link>
-          </div>
-          <div className='flex gap-4 md:gap-8'>
-            <Link href="/product">
-              <div className='flex items-center text-[#f8ffe5]/60 hover:text-[#F8FFE5] transition-all'>
-                <p className='tracking-wide text-xs md:text-sm '>Products</p>
-              </div>
-            </Link>
-            <Link href="/about">
-              <div className='flex items-center text-[#f8ffe5]/60 hover:text-[#F8FFE5] transition-all'>
-                <p className='tracking-wide text-xs md:text-sm '>About Rilstore.</p>
-              </div>
-            </Link>
+    <div className='w-full '>
+      <div className='w-full h-auto flex flex-col items-center shadow-md bg-[#70d6bb] '>
+        <div className='w-full flex justify-center bg-[#6dcfb5] '>
+          <div className='w-full flex pt-2 rounded-t-[10px] justify-between items-center max-w-[1400px]  px-4'>
+            <div>
+              <Link href="/">
+                <p className='text-xl font-semibold tracking-wide text-[#F8FFE5]'>RilStore.</p>
+              </Link>
+            </div>
+            <div className='flex gap-4 md:gap-8'>
+              <Link href="/product">
+                <div className='flex items-center text-[#f8ffe5]/60 hover:text-[#F8FFE5] transition-all'>
+                  <p className='tracking-wide text-xs md:text-sm '>Products</p>
+                </div>
+              </Link>
+              <Link href="/about">
+                <div className='flex items-center text-[#f8ffe5]/60 hover:text-[#F8FFE5] transition-all'>
+                  <p className='tracking-wide text-xs md:text-sm '>About Rilstore.</p>
+                </div>
+              </Link>
+            </div>
           </div>
         </div>
-        <div className='flex items-center justify-between gap-3 px-3 pt-2 pb-3'>
+        <div className='w-full flex items-center justify-between gap-3 px-4 pt-2 pb-3 max-w-[1400px] '>
           <div className='w-full flex justify-center'>
             <Suspense fallback={<div>Loading...</div>}>
               <SearchBar />
