2f34659bf3f2eef07e4865a5fbde7333b1f71f78
ran <randyparadita31@gmail.com>
2025-03-12 19:49:15 +0800

    Add limits data for pagination options

---
+++ src/lib/data/limits.ts
@@ -0,0 +1,22 @@
+export const limits = [
+    {
+        name: '4',
+        params: '4'
+    },
+    {
+        name: '8',
+        params: '8'
+    },
+    {
+        name: '12',
+        params: '12'
+    },
+    {
+        name: '16',
+        params: '16'    
+    },
+    {
+        name: 'all',
+        params: 'all'
+    }
+]
