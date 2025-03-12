2d04ba331d0cd1f8aa39e01d41a93e3f20a0ee50
ran <randyparadita31@gmail.com>
2025-03-12 22:22:20 +0800

    Add CarouselArrowButtons component for navigation in carousel

---
+++ src/components/ui/CarouselArrowButtons.tsx
@@ -0,0 +1,85 @@
+import React, {
+    ComponentPropsWithRef,
+    useCallback,
+    useEffect,
+    useState
+  } from 'react'
+  import { EmblaCarouselType } from 'embla-carousel'
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
+  
+  type UsePrevNextButtonsType = {
+    prevBtnDisabled: boolean
+    nextBtnDisabled: boolean
+    onPrevButtonClick: () => void
+    onNextButtonClick: () => void
+  }
+  
+  export const usePrevNextButtons = (
+    emblaApi: EmblaCarouselType | undefined,
+    onButtonClick?: (emblaApi: EmblaCarouselType) => void
+  ): UsePrevNextButtonsType => {
+    const [prevBtnDisabled, setPrevBtnDisabled] = useState(true)
+    const [nextBtnDisabled, setNextBtnDisabled] = useState(true)
+  
+    const onPrevButtonClick = useCallback(() => {
+      if (!emblaApi) return
+      emblaApi.scrollPrev()
+      if (onButtonClick) onButtonClick(emblaApi)
+    }, [emblaApi, onButtonClick])
+  
+    const onNextButtonClick = useCallback(() => {
+      if (!emblaApi) return
+      emblaApi.scrollNext()
+      if (onButtonClick) onButtonClick(emblaApi)
+    }, [emblaApi, onButtonClick])
+  
+    const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
+      setPrevBtnDisabled(!emblaApi.canScrollPrev())
+      setNextBtnDisabled(!emblaApi.canScrollNext())
+    }, [])
+  
+    useEffect(() => {
+      if (!emblaApi) return
+  
+      onSelect(emblaApi)
+      emblaApi.on('reInit', onSelect).on('select', onSelect)
+    }, [emblaApi, onSelect])
+  
+    return {
+      prevBtnDisabled,
+      nextBtnDisabled,
+      onPrevButtonClick,
+      onNextButtonClick
+    }
+  }
+  
+  type PropType = ComponentPropsWithRef<'button'>
+  
+  export const PrevButton: React.FC<PropType> = (props) => {
+    const { children, ...restProps } = props
+  
+    return (
+      <button
+        type="button"
+        {...restProps}
+      >
+        <IoIosArrowBack size={45} className='rounded-full p-2 bg-zinc-500/10 hover:opacity-70 transition-all'/>
+        {children}
+      </button>
+    )
+  }
+  
+  export const NextButton: React.FC<PropType> = (props) => {
+    const { children, ...restProps } = props
+  
+    return (
+      <button
+        type="button"
+        {...restProps}
+      >
+                <IoIosArrowForward size={45} className='rounded-full p-2 bg-zinc-500/10 hover:opacity-70 transition-all'/>
+        {children}
+      </button>
+    )
+  }
+  
