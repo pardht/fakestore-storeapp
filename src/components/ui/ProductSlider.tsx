7c6cc9a2a59dfe4c1de2c0f593ccb6c808dded09
ran <randyparadita31@gmail.com>
2025-03-13 22:36:43 +0800

    Add label prop to ProductSlider component and enhance layout with improved styling

--- src/components/ui/ProductSlider.tsx
+++ src/components/ui/ProductSlider.tsx
@@ -12,7 +12,7 @@ import {
 } from './CarouselArrowButtons'
 
 
-export default function ProductSlider({ products }: CarouselProps) {
+export default function ProductSlider({ products, label }: CarouselProps) {
     const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true, align: "start" });
     const onNavButtonClick = useCallback((emblaApi: EmblaCarouselType) => {
         const autoplay = emblaApi?.plugins()?.autoplay
@@ -32,18 +32,22 @@ export default function ProductSlider({ products }: CarouselProps) {
         onNextButtonClick
     } = usePrevNextButtons(emblaApi, onNavButtonClick)
     return (
-        <div className='py-4 px-4'>
-            <div ref={emblaRef} className='overflow-clip relative rounded-[10px]'>
-                <div className='flex relative'>
-                    {products.map((product) => (
-                        <div key={product.id} className='flex-[0_0_25%] px-2'>
-                            <ProductCard product={product} />
+        <div className='relative'>
+            <div className='py-4 px-4'>
+                <p className="text-2xl font-semibold py-4 px-2">{label}</p>
+                <div className='py-10 px-4 bg-white rounded-[10px]'>
+                    <div ref={emblaRef} className='overflow-clip relative rounded-[10px]'>
+                        <div className='flex relative'>
+                            {products.map((product) => (
+                                <div key={product.id} className='flex-[0_0_25%] px-2'>
+                                    <ProductCard product={product} />
+                                </div>
+                            ))}
                         </div>
-                    ))}
+                    </div>
                 </div>
-
             </div>
-            <div className="flex justify-between p-5 transition-all ">
+            <div className="absolute right-0 top-0 flex gap-3 p-5 transition-all ">
                 <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
                 <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
             </div>
