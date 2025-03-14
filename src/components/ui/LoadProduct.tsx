6438feea5f479b85c6312c0ea919b6c220aa22a9
ran <randyparadita31@gmail.com>
2025-03-14 21:27:41 +0800

    Export LoadCard component for improved reusability in other parts of the application

--- src/components/ui/LoadProduct.tsx
+++ src/components/ui/LoadProduct.tsx
@@ -29,16 +29,16 @@ export default function LoadProduct() {
   )
 }
 
-function LoadCard() {
+export function LoadCard() {
   return (
     <div className='w-full h-[16rem] md:h-[23rem]  bg-white rounded-[10px] shadow-xl shadow-[#456d62]/10 brightness-[0.95]'>
       <div className='w-full h-full flex flex-col items-center justify-center space-y-10'>
         <CgSpinner className='animate-spin hidden md:block md:w-40 md:h-[13.5rem] text-black/20' />
         <CgSpinner size={100} className='animate-spin md:hidden text-black/20' />
         <div className='w-full flex flex-col justify-start items-start px-3 md:p-5 space-y-2'>
+          <div className='w-[50px] md:w-[40px] p-[5px] md:p-2 bg-black/20 rounded-[10px]'></div>
           <div className='w-full max-w-[160px] md:max-w-[300px] p-[5px] md:p-2 bg-black/15 rounded-[10px]'></div>
           <div className='w-[80px] md:w-[130px] p-[5px] md:p-2 bg-black/5 rounded-[10px]'></div>
-          <div className='w-[50px] md:w-[40px] p-[5px] md:p-2 bg-black/20 rounded-[10px]'></div>
         </div>
       </div>
     </div>
