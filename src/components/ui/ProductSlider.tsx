e2f8321f9fea51e140373b043709d8e52091c4ec
ran <randyparadita31@gmail.com>
2025-03-14 22:24:05 +0800

    Enhance ProductSlider layout and responsiveness with improved spacing and text sizing

--- src/components/ui/ProductSlider.tsx
+++ src/components/ui/ProductSlider.tsx
@@ -39,27 +39,25 @@ export default function ProductSlider({ products, label }: CarouselProps) {
         setRandomizedProducts(randomized)
     }, [products])
     return (
-        <div className='relative'>
-            <div className='py-4 px-4'>
-                <div className='py-4 px-2 flex gap-2'>
-                    <p className="text-2xl font-semibold ">{label}</p>
+        <div className='relative '>
+            <div className='flex flex-col gap-2'>
+                <div className='p-5 flex gap-2 bg-zinc-100 rounded-[10px]'>
+                    <p className="md:text-2xl font-semibold ">{label}</p>
                     <Link href={`/product?category=${label.toLocaleLowerCase()}`}>
-                        <p className="text-lg font-medium text-[#6dcfb5] transition-all hover:opacity-70 ">See More</p>
+                        <p className="text-xs md:text-lg font-medium text-[#6dcfb5] transition-all hover:opacity-70 ">See More</p>
                     </Link>
                 </div>
-                <div className='py-10 px-4 bg-white rounded-[10px]'>
-                    <div ref={emblaRef} className='overflow-clip relative rounded-[10px]'>
-                        <div className='flex relative'>
-                            {randomizedProducts.map((product) => (
-                                <div key={product.id} className='flex-[0_0_25%] px-2'>
-                                    <ProductCard product={product} />
-                                </div>
-                            ))}
-                        </div>
+                <div ref={emblaRef} className='overflow-clip relative rounded-[10px]'>
+                    <div className='flex relative'>
+                        {randomizedProducts.map((product) => (
+                            <div key={product.id} className='flex-[0_0_50%] md:flex-[0_0_25%] px-[4px] md:px-2'>
+                                <ProductCard product={product} />
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
-            <div className="absolute right-0 top-0 flex gap-3 p-5 transition-all ">
+            <div className="absolute right-0 top-0 flex gap-[4px] md:gap-3 p-4 transition-all ">
                 <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
                 <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
             </div>
