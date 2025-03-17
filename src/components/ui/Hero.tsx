ed822d6aa3319fa3b0cadf86b3fe25d892cd4755
ran <randyparadita31@gmail.com>
2025-03-17 22:10:38 +0800

    Refactor Hero component to use state and effect for randomized hero text

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -1,9 +1,17 @@
+"use client"
+
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { heroText } from "@/lib/data/heroText";
 
 export default function Hero() {
-  const hero = heroText.sort(() => Math.random() - 0.5).slice(0, 1)
+  const [hero, setHero] = useState<string[]>()
+
+  useEffect(() => {
+    const randomized = heroText.sort(() => Math.random() - 0.5).slice(0, 1)
+    setHero(randomized)
+  }, [])
+
   return (
     <div className="w-full md:h-full p-4 md:p-6 md:px-8 brightness-[0.95] bg-white  rounded-[10px] ">
       <p className=" font-semibold md:font-black text-[1.5rem] line-clamp-2 md:text-[3rem]">
