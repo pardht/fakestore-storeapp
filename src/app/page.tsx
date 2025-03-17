50ea23816ab8c409f3f2ff65baea54dc3a27576f
ran <randyparadita31@gmail.com>
2025-03-17 23:16:58 +0800

    Remove Hero component and Suspense wrapper from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
 import HomePage from "@/components/HomePage";
-import Hero from "@/components/ui/Hero";
-import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div className="w-full h-auto flex flex-col gap-5 md:gap-10">
-      <Suspense fallback={<p>Loading hero...</p>}>
-        <Hero />
-      </Suspense>
       <HomePage />
     </div >
   );
