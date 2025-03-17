b8584a652d9d5c1eb633df5450102e6f206f001b
ran <randyparadita31@gmail.com>
2025-03-17 21:50:12 +0800

    Add Hero component for displaying hero section in the UI

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -5,34 +5,26 @@ import Link from "next/link";
 import ProductSlider from "./ui/ProductSlider";
 import { CiSearch } from "react-icons/ci";
 import Carousel from "./ui/Carousel";
+import Hero from "./ui/Hero";
 
 export default async function HomePage() {
-  const hero = heroText.sort(() => Math.random() - 0.5).slice(0, 1)
   const productsByCate = await Promise.all(
     categories.sort(() => Math.random() - 0.5).map((category) => getAllProductImproved({ category: category.params.toString() }))
   );
   const products = await getAllProductImproved({ category: "" })
+  const randomSearch = products.sort(() => Math.random() - 0.5).slice(0, 1)
   const carouselProducts = products.filter((product) => product.rating.rate >= 4 && product.rating.count >= 400).sort(() => Math.random() - 0.5).slice(0, 3)
   return (
     <div className="flex flex-col gap-2 md:gap-4">
       <div className=" rounded-[10px] bg-white flex flex-col md:flex-row gap-2 md:gap-4">
-        <div className="w-full md:h-full p-4 md:p-6 md:px-8 brightness-[0.95] bg-white  rounded-[10px] ">
-          <p className=" font-semibold md:font-black text-[1.5rem] line-clamp-2 md:text-[3rem]">
-            {hero}
-          </p>
-          <p className=" font-semibold md:font-black text-[1.5rem] md:text-[3rem] text-[#6dcfb5]">
-            <Link href="/product">
-              Start browsing!
-            </Link>
-          </p>
-        </div>
+        <Hero/>
         <div className="  rounded-[10px]">
           <Carousel products={carouselProducts} />
         </div>
       </div>
       <div className="flex flex-col bg-[#70d6bb] p-4 rounded-[10px] md:flex-row gap-3">
         <div className="flex-1  flex items-center">
-          {products.sort(() => Math.random() - 0.5).slice(0, 1).map((product) => (
+          {randomSearch.map((product) => (
             <Link className="w-full  group" key={product.id} href={`/product?search=${product.title}`}>
               <div className='w-full p-2 md:p-3 flex items-center bg-white shadow-black/10 shadow-sm rounded-[15px] '>
                 <p className="w-full line-clamp-1 text-sm md:text-base md:font-medium group-hover:translate-x-[4px] transition-all">{product.title}</p>
---
+++ src/components/ui/Hero.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import React from 'react'
+import { heroText } from "@/lib/data/heroText";
+
+export default function Hero() {
+  const hero = heroText.sort(() => Math.random() - 0.5).slice(0, 1)
+  return (
+    <div className="w-full md:h-full p-4 md:p-6 md:px-8 brightness-[0.95] bg-white  rounded-[10px] ">
+      <p className=" font-semibold md:font-black text-[1.5rem] line-clamp-2 md:text-[3rem]">
+        {hero}
+      </p>
+      <p className=" font-semibold md:font-black text-[1.5rem] md:text-[3rem] text-[#6dcfb5]">
+        <Link href="/product">
+          Start browsing!
+        </Link>
+      </p>
+    </div>
+  )
+}
