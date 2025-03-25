9abf6ad355016d7f249377a98a7cf9c5569047c4
ran <randyparadita31@gmail.com>
2025-03-25 20:39:30 +0800

    Refactor Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
-import HomePage from "@/components/HomePage";
+import RandomizedSlider from "@/components/RandomizedSlider";
+import Carousel from "@/components/ui/Carousel";
+import Hero from "@/components/ui/Hero";
+import HomeFilter from "@/components/ui/HomeFilter";
+import { categories } from "@/lib/data/categories";
+import { getAllProductImproved } from "@/lib/fetch";
+import { Suspense } from "react";
 
-export default function Home() {
+export default async function Home() {
+  const productsByCate = await Promise.all(
+    categories.map((category) => getAllProductImproved({ category: category.params.toString() }))
+  );
+  const products = await getAllProductImproved({ category: "" })
+  const carouselProducts = products.filter((product) => product.rating.rate >= 4 && product.rating.count >= 400).slice(0, 3)
   return (
-    <div className="w-full h-auto flex flex-col gap-5 md:gap-10">
-      <HomePage />
-    </div >
+    <div className="flex flex-col gap-2 md:gap-4">
+      <div className=" rounded-[10px] bg-white flex flex-col md:flex-row gap-2 md:gap-4">
+        <Suspense fallback={<p>Loading hero...</p>}>
+          <Hero />
+        </Suspense>
+        <div className="  rounded-[10px]">
+          <Carousel products={carouselProducts} />
+        </div>
+      </div>
+      <HomeFilter products={products} />
+      <RandomizedSlider initialProductsByCate={productsByCate} />
+    </div>
   );
 }
